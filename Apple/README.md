733100fd4eee3dacbff8e97e90da2d0b942892ca
Judah Lesser <judahlesser@Judahs-MacBook-Air.local>
2024-08-25 09:50:22 -0400

    use openssl instead of signpass tool

--- Apple/README.md
+++ Apple/README.md
@@ -2,12 +2,13 @@
 Sources and instructions to generate Apple Wallet passes with a UID and barcode on them. You must be on a Mac to generate passes and have Xcode installed.
 
 ## Instructions
+1. Make sure `python3`, `openssl`, and the PIP package `openpyxl` are installed.
 1. Follow the instructions [here](https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/PassKit_PG/YourFirst.html) for how to download the requisite Pass Type ID certificate from the Apple Developer portal.
 1. Double click on the downloaded certificate to add it to Keychain Access.
 1. Double click on the certificate within Keychain Access and note what it says for the "Organizational Unit" within the "Issuer Name" section. For example, it may say "G4".
-1. Download the corresponding Worldwide Developer Relations intermediate certificate from [this page](https://www.apple.com/certificateauthority/) and once again add it to Keychain Access.
-1. Download and compile (with Xcode) the [signpass tool](https://developer.apple.com/services-account/download?path=/iOS/Wallet_Support_Materials/WalletCompanionFiles.zip) (can also be found on the first page linked above). It makes the signing process much easier.
-1. The compiled executable will be in `/Users/yourname/Library/Developer/Xcode/DerivedData/signpass-blahblah/Build/Products/Debug`. Move the executable to `HillelPasses/Apple`.
+1. Download the corresponding Worldwide Developer Relations (WWDR) intermediate certificate from [this page](https://www.apple.com/certificateauthority/) and once again add it to Keychain Access.
+1. Right-click on the Pass Type ID certificate in Keychain Access and export it as `Certificates.p12` to `HillelPasses/Apple` without a password.
+1. Right-click on the WWDR certificate in Keychain Access and export it as `WWDR.pem` to `HillelPasses/Apple` without a password.
 1. Verify that the `passTypeIdentifier` and `teamIdentifier` fields in `pass.json` are correct and match the values in the Apple Developer portal.
 1. Generate a single pass with an invocation like this (quotes are required around the name)
 
--- Apple/genpass.py
+++ Apple/genpass.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-import os
 import sys
 import json
 import shutil
+import random
+import string
 import openpyxl
 import tempfile
 import subprocess
@@ -10,49 +11,52 @@ from pathlib import Path
 
 def main():
     num_args = len(sys.argv)
+    key_pass = random.choices(string.ascii_lowercase, k=10)
     if num_args == 4:
         pass_dir = sys.argv[1]
         xlsx = sys.argv[2]
         dest = sys.argv[3]
-        dest_path = os.path.join("./", dest)
-        if os.path.exists(dest_path) and os.path.isdir(dest_path):
+        dest_path = Path("./", dest)
+        if Path(dest_path).exists() and Path(dest_path).id_dir():
             shutil.rmtree(dest_path)
-        os.mkdir(dest_path)
+        Path(dest_path).mkdir()
         workbook = openpyxl.load_workbook(xlsx)
         worksheet = workbook.active
+        prepare_certs(key_pass)
         for row in worksheet.iter_rows(2, worksheet.max_row, values_only=True):
             name = " ".join([row[0], row[1]])
             id = str(row[2])
             barcode = str(row[3])
-            makepass(pass_dir, name, id, barcode, os.path.join(dest_path, f"{name.replace(' ', '_')}.pkpass"))
+            makepass(pass_dir, name, id, barcode, Path(dest_path, f"{name.replace(' ', '_')}.pkpass"), key_pass)
+        cleanup_certs()
 
     elif num_args == 5:
         pass_dir = sys.argv[1]
         name = sys.argv[2]
         uid = sys.argv[3]
         barcode = sys.argv[4]
-        makepass(pass_dir, name, uid, barcode, f"./{uid}.pkpass")
+        prepare_certs(key_pass)
+        makepass(pass_dir, name, uid, barcode, f"./{uid}.pkpass", key_pass)
+        cleanup_certs()
         
     else:
         print("Invalid arguments")
         return
     
-def makepass(pass_dir, name, uid, barcode, dest):
-    #if len(uid) != 9:
-    #    print("UID must be 9 digits")
-    #    return
+def makepass(pass_dir, name, uid, barcode, dest, key_pass):
+    if len(uid) != 9:
+       print("UID must be 9 digits")
+       return
     if len(barcode) != 14:
         print("Barcode must be 14 digits")
         return
-    try:
-        os.remove(os.path.join(pass_dir, ".DS_Store"))
-    except OSError:
-        pass
+    Path(pass_dir, ".DS_Store").unlink(missing_ok=True)
+    
     # Make temp directory to make pass in
     with tempfile.TemporaryDirectory() as dst:
         just_folder = Path(pass_dir).name
         newdir = Path(dst, just_folder)
-        Path.mkdir(newdir)
+        newdir.mkdir()
 
         # copy pass source to temp directory
         shutil.copytree(pass_dir, newdir, dirs_exist_ok=True) 
@@ -69,9 +73,31 @@ def makepass(pass_dir, name, uid, barcode, dest):
 
         with open(Path(newdir, 'pass.json'), 'w') as pass_file:
             json.dump(data, pass_file)
-        
-        subprocess.run(['./signpass', '-p', newdir]) # run pass signer tool
-        shutil.move(Path(dst, f'{os.path.splitext(just_folder)[0]}.pkpass'), dest) # copy created pass back to current directory
+
+        # create manifest file
+        manifest = {}
+        for file in newdir.iterdir():
+            result = subprocess.run(['openssl', 'sha1', file.absolute()], capture_output=True)
+            sha1 = result.stdout.decode('ascii').split()[1]
+            manifest[file.name] = sha1
+        with open(Path(newdir, 'manifest.json'), 'w') as manifest_file:
+            json.dump(manifest, manifest_file)
+        print(f"Wrote manifest {manifest}")
+
+        # See here for the openssl commands: https://www.kodeco.com/2855-beginning-passbook-in-ios-6-part-1-2/page/4
+        subprocess.run(['openssl', 'smime', '-binary', '-sign', '-certfile', 'WWDR.pem',
+                    '-signer', 'cert.pem',  '-in', Path(newdir, 'manifest.json'),
+                    '-out', Path(newdir, 'signature'), '-outform', 'DER', '-passin', f'pass:{key_pass}'])
+
+        subprocess.run(['zip', '-q', '-j', '-r', dest] + [str(p) for p in newdir.iterdir()])
+
+def prepare_certs(key_pass):
+    pass_type_id_cert = 'Certificates.p12'
+    subprocess.run(['openssl', 'pkcs12', '-in', pass_type_id_cert, 
+                    '-clcerts', '-out', 'cert.pem', '-passin', 'pass:', '-passout', f'pass:{key_pass}'])
+
+def cleanup_certs():
+    Path('cert.pem').unlink()
 
 if __name__ == "__main__":
     main()
